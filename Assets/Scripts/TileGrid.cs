aba9484169d2ed71a72183c6d9ed73a106f80e50
vasupatel <vasurabadiya5@gmail.com>
2024-02-29 15:59:00 +0530

    Logic done

--- Assets/Scripts/TileGrid.cs
+++ Assets/Scripts/TileGrid.cs
@@ -11,80 +11,60 @@ public class TileGrid : MonoBehaviourPunCallbacks
         cross
     }
 
-    [SerializeField] GameObject GameOverPanel, Parent;
+    [SerializeField] GameObject GameOverPanel, Parent, FirstPlayerPanel;
     [SerializeField] PlayerMode[] AllplayerMode;
     [SerializeField] PlayerMode playerMode;
     [SerializeField] Sprite ZeroSprite, CrossSprite;
     public static TileGrid instance;
-    private static bool isPlayerTurn = true; // Flag to track whose turn it is
+    private bool isPlayerTurn = true;
+    int MoveCount=0;
 
     private void Awake()
     {
         instance = this;
     }
-    //public void UserMove(GameObject g, int no)
-    //{
-    //    if (!isPlayerTurn) // Check if it's not the player's turn
-    //    {
-
-
-    //        if (playerMode == PlayerMode.zero)
-    //        {
-    //            g.GetComponent<SpriteRenderer>().sprite = CrossSprite;
-    //            g.GetComponent<BoxCollider2D>().enabled = false;
-    //            AllplayerMode[no] = PlayerMode.cross;
-    //            playerMode = PlayerMode.cross; // Update playerMode to cross before RPC call
-
-    //            photonView.RPC("SyncMove", RpcTarget.Others, no, (int)PlayerMode.cross);
-    //        }
-    //        else
-    //        {
-    //            g.GetComponent<SpriteRenderer>().sprite = ZeroSprite;
-    //            g.GetComponent<BoxCollider2D>().enabled = false;
-    //            AllplayerMode[no] = PlayerMode.zero;
-    //            playerMode = PlayerMode.zero; // Update playerMode to zero before RPC call
-
-    //            photonView.RPC("SyncMove", RpcTarget.Others, no, (int)PlayerMode.zero);
-    //        }
-    //        CheckWinOrNot();
-    //    }
-    //    isPlayerTurn = !isPlayerTurn; // Toggle isPlayerTurn to switch to the other player's turn
-    //}
+    private void Start()
+    {
+      
+    }
     private void Update()
     {
         Debug.Log("Turn = "+isPlayerTurn);
     }
     public void UserMove(GameObject g, int no)
     {
-        if (!isPlayerTurn) // Check if it's not the player's turn
+        MoveCount++;
+        if (!isPlayerTurn)
         {
-            isPlayerTurn = true;
-            Debug.Log("IF called");
-            g.GetComponent<SpriteRenderer>().sprite = CrossSprite;
-            g.GetComponent<BoxCollider2D>().enabled = false;
-            AllplayerMode[no] = PlayerMode.cross;
-            playerMode = PlayerMode.cross; // Update playerMode to cross before RPC call
-
-            photonView.RPC("SyncMove", RpcTarget.Others, no, (int)PlayerMode.cross);
-            CheckWinOrNot();
+            Debug.Log("Player's turn: Cross");
+            PerformMove(g, no, PlayerMode.cross);
+         //   FirstPlayerPanel.SetActive(true);
         }
         else
         {
-            isPlayerTurn = false;
-            Debug.Log("Else called");
-            g.GetComponent<SpriteRenderer>().sprite = ZeroSprite;
-            g.GetComponent<BoxCollider2D>().enabled = false;
-            AllplayerMode[no] = PlayerMode.zero;
-            playerMode = PlayerMode.zero; // Update playerMode to zero before RPC call
+            Debug.Log("Player's turn: Zero");
+            PerformMove(g, no, PlayerMode.zero);
+          //  FirstPlayerPanel.SetActive(true);
 
-            photonView.RPC("SyncMove", RpcTarget.Others, no, (int)PlayerMode.zero);
-            CheckWinOrNot();
         }
+    }
+    private void PerformMove(GameObject g, int index, PlayerMode mode)
+    {
+        g.GetComponent<SpriteRenderer>().sprite = mode == PlayerMode.cross ? CrossSprite : ZeroSprite;
+        g.GetComponent<BoxCollider2D>().enabled = false;
+        AllplayerMode[index] = mode;
+        playerMode = mode; // Update playerMode
+
+        photonView.RPC("SyncMove", RpcTarget.AllBuffered, index, (int)mode);
+        CheckWinOrNot();
     }
 
     [PunRPC]
     private void SyncMove(int index, int mode)
     {
+        isPlayerTurn = !isPlayerTurn;
+        Debug.Log("Player switched = " + isPlayerTurn);
+
         AllplayerMode[index] = (PlayerMode)mode;
         GameObject tile = Parent.transform.GetChild(index).gameObject;
         tile.GetComponent<BoxCollider2D>().enabled = false;
@@ -92,16 +72,42 @@ public class TileGrid : MonoBehaviourPunCallbacks
         if ((PlayerMode)mode == PlayerMode.cross)
         {
             tile.GetComponent<SpriteRenderer>().sprite = CrossSprite;
+
         }
         else if ((PlayerMode)mode == PlayerMode.zero)
         {
             tile.GetComponent<SpriteRenderer>().sprite = ZeroSprite;
         }
+        if (isPlayerTurn) // If it's now the current player's turn
+        {
+            if (photonView.IsMine) // If it's the local player's turn
+            {
+                Debug.Log("It's my turn. Showing panel...");
+                FirstPlayerPanel.SetActive(false); // Show panel for the local player
+            }
+            else
+            {
+                Debug.Log("It's opponent's turn. Hiding panel...");
+                FirstPlayerPanel.SetActive(true); // Hide panel for the opponent
+            }
+        }
+        else // If it's not the current player's turn
+        {
+            if (photonView.IsMine) // If it's the local player's turn
+            {
+                Debug.Log("It's my opponent's turn. Hiding panel...");
+                FirstPlayerPanel.SetActive(true); // Hide panel for the local player
+            }
+            else
+            {
+                Debug.Log("It's my turn. Showing panel...");
+                FirstPlayerPanel.SetActive(false); // Show panel for the opponent
+            }
+        }
 
         CheckWinOrNot();
-
-        isPlayerTurn = true; // Switch back to the first player's turn
     }
+
     public void CheckWinOrNot()
     {
         int[,] data = new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 } };
@@ -113,6 +119,16 @@ public class TileGrid : MonoBehaviourPunCallbacks
                 Debug.Log("Player win = " + playerMode + "...." + i);
                 GameOverPanel.SetActive(true);
             }
+        }
+        if (MoveCount == 9)
+        {
+            MoveCount = 0;
+            GameOverPanel.SetActive(true);
+            Debug.Log("No move available");
+        }
+        else
+        {
+            Debug.Log("Moves available = "+MoveCount);
         }
     }
     public void OnClick_HomeBtn()
